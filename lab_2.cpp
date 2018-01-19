4ace2727b792521ce4b1a2d76f9fadf29225a3c4
b-biplab <31165079+b-biplab@users.noreply.github.com>
2018-01-19 13:26:20 +0530

    Create lab_2.cpp

---
+++ lab_2.cpp
@@ -0,0 +1,187 @@
+
+
+/* Biplab Behera (1711042)
+ * Computer Science {CS142}
+ * Lab-2 Assignment
+ */
+
+#include<iostream>
+using namespace std;
+
+/*
+ * Contains a data storage unit and a pointer to the next
+ * element of the linked list.
+ */
+
+class node
+{
+        public:
+	int data;
+	node *next;
+};
+
+/*
+ * Class for Singly Linked List data structure
+ * Pointers to the first and last element of the list
+ */
+class list
+{
+		private:
+		node *head, *tail;
+		public:
+		list()
+		{
+			head=NULL;
+			tail=NULL;
+		}
+
+
+
+	void createnode(int value)
+	/*
+         * Inserts the value stored at the end of the list.
+         */
+	                {
+			node *temp=new node;
+			temp->data=value;
+			temp->next=NULL;
+			if(head==NULL)
+			{
+				head=temp;
+				tail=temp;
+				temp=NULL;
+			}
+			else
+			{
+				tail->next=temp;
+				tail=temp;
+			}
+		}
+		/*
+                 * Inserts the value stored at the start of the list.
+                 */
+		void insert_start(int value)
+		{
+			node *temp=new node;
+			temp->data=value;
+			temp->next=head;
+			head=temp;
+		}
+	        /*
+                 * Inserts the value stored at the any position of the list.
+                 */
+		void insert_position(int pos, int value)
+		{
+			node *pre=new node;
+			node *cur=new node;
+			node *temp=new node;
+			cur=head;
+			for(int i=1;i<pos;i++)
+			{
+				pre=cur;
+				cur=cur->next;
+			}
+			temp->data=value;
+			pre->next=temp;
+			temp->next=cur;
+		}
+	        /*
+                 * Deletes the value stored at the first of the list.
+                 */
+		void delete_first()
+		{
+			node *temp=new node;
+			temp=head;
+			head=head->next;
+			delete temp;
+		}
+	        /*
+                 * Deletes the value stored at the last of the list.
+                 */
+		void delete_last()
+		{
+			node *current=new node;
+			node *previous=new node;
+			current=head;
+			while(current->next!=NULL)
+			{
+				previous=current;
+				current=current->next;
+			}
+			tail=previous;
+			previous->next=NULL;
+			delete current;
+		}
+	        /*
+                 * Deletes the value stored at the position of the list.
+                 */
+		void delete_position(int pos)
+		{
+			node *current=new node;
+			node *previous=new node;
+			current=head;
+			for(int i=1;i<pos;i++)
+			{
+				previous=current;
+				current=current->next;
+			}
+			previous->next=current->next;
+		}
+	/*
+                 * Displays the value stored of the list.
+                 */
+	void display()
+		{
+			node *temp=new node;
+			temp=head;
+			while(temp!=NULL)
+			{
+				cout<<temp->data<<"\t";
+				temp=temp->next;
+			}
+		}
+};
+int main()
+{
+	list obj;
+	obj.createnode(10);
+	obj.createnode(20);
+	obj.createnode(30);
+	obj.createnode(40);
+	cout<<"\n--------------------------------------------------\n";
+	cout<<"---------------Displaying All nodes---------------";
+	cout<<"\n--------------------------------------------------\n";
+	obj.display();
+	cout<<"\n--------------------------------------------------\n";
+	cout<<"-----------------Inserting At End-----------------";
+	cout<<"\n--------------------------------------------------\n";
+	obj.createnode(50);
+	obj.display();
+	cout<<"\n--------------------------------------------------\n";
+	cout<<"----------------Inserting At Start----------------";
+	cout<<"\n--------------------------------------------------\n";
+	obj.insert_start(05);
+	obj.display();
+	cout<<"\n--------------------------------------------------\n";
+	cout<<"-------------Inserting At Particular--------------";
+	cout<<"\n--------------------------------------------------\n";
+	obj.insert_position(5,35);
+	obj.display();
+	cout<<"\n--------------------------------------------------\n";
+	cout<<"----------------Deleting At Start-----------------";
+	cout<<"\n--------------------------------------------------\n";
+	obj.delete_first();
+	obj.display();
+	cout<<"\n--------------------------------------------------\n";
+	cout<<"-----------------Deleing At End-------------------";
+	cout<<"\n--------------------------------------------------\n";
+	obj.delete_last();
+	obj.display();
+	cout<<"\n--------------------------------------------------\n";
+	cout<<"--------------Deleting At Particular--------------";
+	cout<<"\n--------------------------------------------------\n";
+	obj.delete_position(4);
+	obj.display();
+	cout<<"\n--------------------------------------------------\n";
+	return 0;
+}
